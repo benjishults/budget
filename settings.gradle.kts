fa676daabbe481b25fa985b6fd231b40865859e9
Benji Shults <14838863+benjishults@users.noreply.github.com>
2024-11-11 13:37:20 -0600

    agree to gradle scan terms

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
 rootProject.name = "budget"
 
+plugins {
+    id("com.gradle.develocity") version ("3.18.1")
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
+
