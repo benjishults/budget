af69809676df79ccd7eefa82033d3e0ba804daba
Benji Shults <14838863+benjishults@users.noreply.github.com>
2024-12-04 15:25:27 -0600

    new test docker setup worked locally

--- ci/10setupDbAsAdmin.sql
+++
@@ -1,6 +0,0 @@
-create database budget;
--- create user budget password 'budget';
--- GRANT ALL PRIVILEGES ON DATABASE budget TO budget;
--- GRANT USAGE ON SCHEMA public TO budget;
--- alter database budget owner to budget;
-create user test password 'test';
---
+++ ci/init.sql
@@ -0,0 +1,4 @@
+-- create database budget;
+-- create user test password 'test';
+CREATE SCHEMA if not exists clean_after_test authorization test;
+CREATE SCHEMA if not exists test authorization test;
