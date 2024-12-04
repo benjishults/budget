2ecabdfa1198511270be801cfe699f852d91f39e
Benji Shults <14838863+benjishults@users.noreply.github.com>
2024-12-04 15:27:49 -0600

    clean up comment code

--- ci/init.sql
+++ ci/init.sql
@@ -1,4 +1,2 @@
--- create database budget;
--- create user test password 'test';
 CREATE SCHEMA if not exists clean_after_test authorization test;
 CREATE SCHEMA if not exists test authorization test;
